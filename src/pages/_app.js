c9860eb5a24e0859bcf5daa5b068fb0eded79e26
Axrous <argasatya16@gmail.com>
2023-08-06 17:05:12 +0700

    feat:progress 30%

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto, Exo, Montserrat } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "400",
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+const exo = Exo({
+  weight: "700",
+  variable: "--font-exo",
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  weight: "400",
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${roboto.variable} ${exo.variable} ${montserrat.variable}  font-sans`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,120 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import profilePicture from "../../public/images/arga.jpeg";
+import PHP from "../../public/images/logos/php.png";
+import go from "../../public/images/logos/go.png";
+import js from "../../public/images/logos/js.png";
+import py from "../../public/images/logos/python.png";
+import java from "../../public/images/logos/java.png";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-[#F9F6EE]">
+      <nav className="bg-[#7FB3D5] mt-0 fixed w-full z-10 top-0">
+        <div className="w-6/12 mx-auto flex">
+          <div className="w-6/12">
+            <ul className={`flex py-4 justify-around font-roboto`}>
+              <li>
+                <a href="#home">HOME</a>
+              </li>
+              <li>
+                <a href="#about">ABOUT</a>
+              </li>
+              <li>SKILLS</li>
+              <li>PROJECTS</li>
+              <li>CONTACTS</li>
+            </ul>
+          </div>
+          <div className="w-6/12 flex justify-end pr-8 my-auto">
+            <a href="#" className="hover:text-red-500">
+              LOGO
+            </a>
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="w-6/12 flex mx-auto h-screen" id="home">
+        <div className="flex my-auto">
+          <div>
+            <Image
+              src={profilePicture}
+              width={200}
+              className="rounded-full"
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="ml-5 w-10/12 border-l-[12px] border-l-[#7FB3D5] pl-2 py-1 my-auto">
+            <h1 className="text-5xl font-exo font-bold tracking-widest">
+              ARGA SATYA MULYONO
+            </h1>
+            <h2 className="text-xl font-montserrat w-8/12">
+              A human who wants to become a proficient programmer soon. Wish me
+              Luck!
+            </h2>
+          </div>
+        </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="w-6/12 flex mx-auto h-screen" id="about">
+        <div className="w-full my-auto pb-40 mx-auto">
+          <div className="text-center">
+            <h1 className="text-5xl font-exo mx-auto text-center inline-block border-b-[12px] border-b-[#7FB3D5]">
+              ABOUT ME
+            </h1>
+          </div>
+          <div className="w-10/12 mx-auto mt-28">
+            <h2 className="text-3xl font-montserrat font-semibold">
+              Hello, I am Arga Satya Mulyono.
+            </h2>
+            <p className="font-montserrat text-lg w-10/12 mt-2">
+              A fresh graduate who is exploring Backe-End. This portfolio
+              contains my projects and developments. I am passionate about
+              contributing to software development. dan seterusnya
+            </p>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className="w-6/12 flex mx-auto h-screen" id="about">
+        <div className="w-full my-auto pb-40 mx-auto">
+          <div className="text-center">
+            <h1 className="text-5xl font-exo mx-auto text-center inline-block border-b-[12px] border-b-[#7FB3D5]">
+              SKILLS
+            </h1>
+          </div>
+          <div className="w-11/12 mx-auto mt-28 grid grid-cols-4 gap-4">
+            <div className="text-center flex flex-col justify-between h-52 py-4 px-4 mx-auto rounded-lg shadow-2xl grayscale">
+              <Image src={PHP} width={150} alt="Image" className="my-auto" />
+              {/* <p className="text-xl font-montserrat">PHP</p> */}
+            </div>
+            <div className="text-center flex flex-col justify-between h-52 py-4 px-4 mx-auto rounded-lg shadow-2xl grayscale">
+              <Image src={go} width={150} alt="Image" className="my-auto" />
+              {/* <p className="text-xl font-montserrat">GOLANG</p> */}
+            </div>
+            <div className="text-center flex flex-col justify-between h-52 py-4 px-4 mx-auto rounded-lg shadow-2xl grayscale">
+              <Image src={js} width={150} alt="Image" className="my-auto" />
+              {/* <p className="text-xl font-montserrat">JavaScript</p> */}
+            </div>
+            <div className="text-center flex flex-col justify-between h-52 py-4 px-4 mx-auto rounded-lg shadow-2xl grayscale">
+              <Image src={py} width={150} alt="Image" className="my-auto" />
+              {/* <p className="text-xl font-montserrat">JavaScript</p> */}
+            </div>
+            <div className="text-center flex flex-col justify-between h-52 py-4 px-4 mx-auto rounded-lg shadow-2xl grayscale">
+              <Image src={java} width={150} alt="Image" className="my-auto" />
+              {/* <p className="text-xl font-montserrat">JavaScript</p> */}
+            </div>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="w-6/12 flex mx-auto h-screen" id="about">
+        <div className="w-full my-auto pb-40 mx-auto">
+          <div className="text-center">
+            <h1 className="text-5xl font-exo mx-auto text-center inline-block border-b-[12px] border-b-[#7FB3D5]">
+              PROJECTS
+            </h1>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
