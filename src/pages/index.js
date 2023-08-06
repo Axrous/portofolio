d5ee8f85e2eaa858f6021883b70fad538d399be3
Axrous <argasatya16@gmail.com>
2023-08-06 23:04:11 +0700

    feat:add projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -107,12 +107,161 @@ export default function Home() {
       </section>
 
       <section className="w-6/12 flex mx-auto h-screen" id="about">
-        <div className="w-full my-auto pb-40 mx-auto">
+        <div className="w-full my-auto mx-auto">
           <div className="text-center">
             <h1 className="text-5xl font-exo mx-auto text-center inline-block border-b-[12px] border-b-[#7FB3D5]">
               PROJECTS
             </h1>
           </div>
+          <div className="mt-20 flex justify-evenly">
+            <div class="w-3/12 bg-white border border-gray-200 rounded-lg shadow">
+              <Image
+                src={java}
+                alt="project-image"
+                className="p-2 border-b-2"
+              />
+
+              <div class="p-2">
+                <h5 class="mb-2 text-xl font-montserrat font-bold tracking-tight text-gray-900 ">
+                  Mae Kos
+                </h5>
+
+                <p class="mb-3 font-montserrat text-gray-700 ">
+                  Project manajemen pemesanan kamar kos, dibuat menggunakan
+                  Laravel, MySQL, InertiaJS.
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center   rounded-lg hover:bg-[#7FB3D5] focus:ring-4 focus:outline-none focus:ring-blue-300"
+                >
+                  Learn More
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div class="w-3/12 bg-white border border-gray-200 rounded-lg shadow">
+              <Image
+                src={java}
+                alt="project-image"
+                className="p-2 border-b-2"
+              />
+
+              <div class="p-2">
+                <h5 class="mb-2 text-xl font-montserrat font-bold tracking-tight text-gray-900 ">
+                  Mae Kos
+                </h5>
+
+                <p class="mb-3 font-montserrat text-gray-700 ">
+                  Project manajemen pemesanan kamar kos, dibuat menggunakan
+                  Laravel, MySQL, InertiaJS.
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center   rounded-lg hover:bg-[#7FB3D5] focus:ring-4 focus:outline-none focus:ring-blue-300"
+                >
+                  Learn More
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div class="w-3/12 bg-white border border-gray-200 rounded-lg shadow">
+              <Image
+                src={java}
+                alt="project-image"
+                className="p-2 border-b-2"
+              />
+
+              <div class="p-2">
+                <h5 class="mb-2 text-xl font-montserrat font-bold tracking-tight text-gray-900 ">
+                  Mae Kos
+                </h5>
+
+                <p class="mb-3 font-montserrat text-gray-700 ">
+                  Project manajemen pemesanan kamar kos, dibuat menggunakan
+                  Laravel, MySQL, InertiaJS.
+                </p>
+                <a
+                  href="#"
+                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center   rounded-lg hover:bg-[#7FB3D5] focus:ring-4 focus:outline-none focus:ring-blue-300"
+                >
+                  Learn More
+                  <svg
+                    class="w-3.5 h-3.5 ml-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <a href="#" className="text-lg flex mt-10 justify-center">
+            See More
+            <svg
+              className="w-4 h-4 ml-2 my-auto"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+      </section>
+
+      <section className="w-6/12 flex mx-auto h-screen" id="about">
+        <div className="w-full my-auto pb-40 mx-auto">
+          <div className="text-center">
+            <h1 className="text-5xl font-exo mx-auto text-center inline-block border-b-[12px] border-b-[#7FB3D5]">
+              CONTACTS
+            </h1>
+          </div>
+          <div className=""></div>
         </div>
       </section>
     </main>
